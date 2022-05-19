5ab38f16e0db0c8af90e8b822254164eb04c0d03
Andre Welsh <andre.welsh.dev@gmail.com>
2022-05-19 06:40:28 -0400

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/">Bits & Pieces</a>
+      </header>
+      <main>List Products</main>
+    </div>
+  );
+}
+
+export default App;
